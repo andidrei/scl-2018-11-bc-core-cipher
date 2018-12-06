ae4d535abce716eaa2431e3e8b5cf4cef3685e75
andidrei <andrea.grz89@gmail.com>
2018-12-06 07:48:21 -0300

    agregar output

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-/* Acá va tu código */
+function codificar() {
+
+  let text = document.getElementById("texto").value;
+  let offset = document.getElementById("desplazar").value;
+
+  let resultadoFinal = cipher.encode(text, offset);
+  
+  document.getElementById("textReturn").innerHTML = resultadoFinal;
+  return resultadoFinal;
+}
+
+function decodificar() {
+  let textD = document.getElementById("texto").value;
+  let offsetD = document.getElementById("desplazar").value;
+
+  let resultadoFinalD = cipher.decode(textD, offsetD);
+
+  document.getElementById("textReturn").innerHTML = resultadoFinalD;
+  return resultadoFinalD;
+}
